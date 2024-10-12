125331437b0ba044b2ba01bc77a12e2123665516
mickamy <tetsuro@mickamy.com>
2024-10-12 22:48:39 +0900

    permission: add sudo

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 if command -v apt-get &> /dev/null; then
-  apt-get update
-  apt-get install -y nodejs npm
+  sudo apt-get update
+  sudo apt-get install -y nodejs npm
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
