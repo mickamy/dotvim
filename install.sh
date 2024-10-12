3dd7350b6d71985746a105b802b8160397fc516c
mickamy <tetsuro@mickamy.com>
2024-10-12 22:44:51 +0900

    npm: install nodejs and npm on execute install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+if command -v apt-get &> /dev/null; then
+  apt-get update
+  apt-get install -y nodejs npm
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/pack/plugins/start/coc.nvim" && npm ci && cd -
