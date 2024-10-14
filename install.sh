ba916ffd94db89a651584a4bb618d02cbabedeb8
mickamy <tetsuro@mickamy.com>
2024-10-14 18:24:04 +0900

    perf: install node only if not exists

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ if command -v apt &> /dev/null; then
   sudo add-apt-repository -y ppa:jonathonf/vim
   sudo apt update
   sudo apt install -y vim
-  curl -sL install-node.vercel.app/lts | sudo bash -s -- -y
-
   sudo ln -s "$HOME/.vim" /root/.vim
+fi
+
+# Install latest nodejs
+if [ ! -x "$(command -v node)" ]; then
+    curl -sL install-node.vercel.app/lts | sudo bash -s -- -y
+    export PATH="/usr/local/bin/:$PATH"
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
